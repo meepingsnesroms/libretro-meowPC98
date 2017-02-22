dd86dcf759b8d8a733672e8da4c311514b36ea1b
meepingsnesroms <guicrith@gmail.com>
2017-02-21 16:48:28 -0800

    Start shim library, clean up configs

--- libretro/compiler.h
+++ libretro/compiler.h
@@ -28,27 +28,30 @@ typedef	uint16_t		UINT16;
 typedef	int32_t		SINT32;
 typedef	uint32_t		UINT32;
 
+typedef  int32_t*    INTPTR;
+
+typedef bool BOOL;
+#ifndef	TRUE
+#define	TRUE	true
+#endif
+#ifndef	FALSE
+#define	FALSE	false
+#endif
+
+//#define PTR_TO_UINT32(p)	((UINT32)GPOINTER_TO_UINT(p))
+//#define UINT32_TO_PTR(v)	GUINT_TO_POINTER((UINT32)(v))
+
 #define	BRESULT				UINT
 #define	OEMCHAR				char
 #define	OEMTEXT(string)	string
 #define	OEMSPRINTF			sprintf
 #define	OEMSTRLEN			strlen
 
+#define	SPRINTF		sprintf
+#define	STRLEN		strlen
+#define	GETRAND()	random()
+
 #define SIZE_VGA
-#if !defined(SIZE_VGA)
-#define	RGB16		UINT32
-#define	SIZE_QVGA
-#endif
-
-typedef bool BOOL;
-
-#ifndef	TRUE
-#define	TRUE	true
-#endif
-
-#ifndef	FALSE
-#define	FALSE	false
-#endif
 
 #ifndef	MAX_PATH
 #define	MAX_PATH	256
@@ -71,6 +74,16 @@ typedef bool BOOL;
 #define	FillMemory(a, b, c)	memset((a), (c), (b))
 #endif
 
+#ifndef	roundup
+#define	roundup(x, y)	((((x)+((y)-1))/(y))*(y))
+#endif
+
+#ifndef	NELEMENTS
+#define	NELEMENTS(a)	((int)(sizeof(a) / sizeof(a[0])))
+#endif
+
+#define	msgbox(title, msg)	toolkit_messagebox(title, msg);
+
 #include "common.h"
 #include "milstr.h"
 #include "_memory.h"
@@ -84,24 +97,66 @@ typedef bool BOOL;
 #define	SPRINTF				sprintf
 #define	STRLEN				strlen
 
-#define	VERMOUTH_LIB
-#define	SOUND_CRITICAL
 
-#define	SUPPORT_UTF8
-
-#define	SUPPORT_16BPP
 #define	MEMOPTIMIZE		2
 
+#define	VERMOUTH_LIB
 #define  SOUND_CRITICAL
 #define	SOUNDRESERVE	100
-
-#define	SUPPORT_CRT15KHZ
-#define	SUPPORT_HOSTDRV
 #define	SUPPORT_SWSEEKSND
+
+#define	SCREEN_BPP		16
+
+
+//extras
+#define	SUPPORT_EUC
+#define	SUPPORT_UTF8
+
+#define	SUPPORT_CRT31KHZ
+#define	SUPPORT_SWSEEKSND
+#define  SUPPORT_PC9821
+
+#if defined(SUPPORT_PC9821)
+#define	CPUCORE_IA32
+#define	IA32_PAGING_EACHSIZE
+#define	SUPPORT_PC9801_119
+#endif
+
+#define	SUPPORT_PC9861K
+#define	SUPPORT_SOFTKBD		0
+#define  SUPPORT_S98
+//#define  SUPPORT_WAVEREC
+//#define  SUPPORT_RECVIDEO
+#define	SUPPORT_KEYDISP
+//#define	SUPPORT_MEMDBG32
+#define	SUPPORT_HOSTDRV
+#define	SUPPORT_IDEIO
 #define	SUPPORT_SASI
 #define	SUPPORT_SCSI
 
-#define SUPPORT_ARC
-// #define SUPPORT_ZLIB
+#define	SUPPORT_RESUME
+#define	SUPPORT_STATSAVE	10
+#define	SUPPORT_ROMEO
 
-#define	SCREEN_BPP		16
+#define  SOUND_CRITICAL
+#define	SOUNDRESERVE	100
+#define	SUPPORT_EXTERNALCHIP
+
+#define  SUPPORT_ARC
+#define  SUPPORT_ZLIB
+
+#define	SUPPORT_16BPP
+#define	SUPPORT_NORMALDISP
+
+//outdated things to ignore
+#define	FASTCALL
+#define	CPUCALL
+#define	MEMCALL
+#define	DMACCALL
+#define	IOOUTCALL
+#define	IOINPCALL
+#define	SOUNDCALL
+#define	VRAMCALL
+#define	SCRNCALL
+#define	VERMOUTHCL
+
